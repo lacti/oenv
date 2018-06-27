cfef050614f9599d96b3f46b560e40490ecf902c
jaeyoung.choi <lactrious@gmail.com>
2018-06-27 14:14:10 +0900

    Export as module

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ const fs = require('fs');
 const path = require('path');
 const argv = require('yargs').argv;
 
-const output = argv.output || argv.o || 'oenv.js';
-const outputDir = argv.outputDir || argv.d || '.';
+const oenv = () => {
+  const output = argv.output || argv.o || 'oenv.js';
+  const outputDir = argv.outputDir || argv.d || '.';
 
-const vars = [`// This file is auto-generated: ${new Date().toString()}`];
-for (const pair of argv._) {
-  const [key, value] = pair.split('=');
-  vars.push(`process.env.${key} = "${value}";`);
+  const vars = [`// This file is auto-generated: ${new Date().toString()}`];
+  for (const pair of argv._) {
+    const [key, value] = pair.split('=');
+    vars.push(`process.env.${key} = "${value}";`);
+  }
+  const outputPath = path.join(outputDir, output);
+  const content = vars.join('\n');
+  if (argv.v || argv.verbose) {
+    console.log(content);
+  }
+  fs.writeFileSync(outputPath);
 }
-const outputPath = path.join(outputDir, output);
-const content = vars.join('\n');
-if (argv.v || argv.verbose) {
-  console.log(content);
+module.exports = oenv;
+
+if (require.main === module) {
+  oenv();
 }
-fs.writeFileSync(outputPath);
 
